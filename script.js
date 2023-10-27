ad66217c85033f2df685af43d572cb47cf799349
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-10-27 21:56:46 +0200

    fork one database or mor html pages

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /**
  * Global variables:
  * -> Array -> color database (represents the 3 colors types possible)
+ * -> Color type 
  * -> Button
  * -> Color
  */
@@ -11,18 +12,40 @@ let colors = [
     '##efefef'
 ];
 
+
+
+let colorType = document.querySelectorAll('a');
+console.log(colorType);
+
 let btn = document.getElementById('btn');
 
-let color = document.querySelector('.color-name-type');
+let colorName = document.querySelector('.color-name');
 
+
+/**
+ * EventListener on a 'a' element -> starts function at click
+ * -> depending which 'a' selected, different array is used
+ */
+
+
+/**
+ * EventListener on a 'button' element  -> starts function at click
+ */
 
 btn.addEventListener('click', function() {
     let randomNumber = generateRandomNumber();
     console.log(randomNumber);
-    document.querySelector('body').style.backgroundColor = 'blue';
+    document.querySelector('body').style.backgroundColor = colors[randomNumber];
+    colorName.textContent = colors[randomNumber];
 });
 
 
+
+/**
+ * Generating random number
+ * @returns rendom number -> depending on arrays length
+ */
+
 function generateRandomNumber() {
-    return Math.floor(Math.random() * 11);
+    return Math.floor(Math.random() * colors.length);
 }
